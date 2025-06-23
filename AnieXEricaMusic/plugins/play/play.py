dfad2885ddab7b7db56bd141bc340d6e27ac45b3
slayer <meerutuniversity996@gmail.com>
2025-06-23 16:21:25 +0530

    Update play.py

--- AnieXEricaMusic/plugins/play/play.py
+++ AnieXEricaMusic/plugins/play/play.py
@@ -1,3 +1,4 @@
+
 import random
 import string
 
@@ -53,16 +54,8 @@ async def play_commnd(
     url,
     fplay,
 ):
-if channel:
-    mystic = await message.reply_text(_["play_2"].format(channel))
-else:
-    play_1 = _["play_1"]
-    if isinstance(play_1, dict) and "sticker" in play_1:
-        mystic = await message.reply_cached_media(file_id=play_1["sticker"])
-    else:
-        mystic = await message.reply_text(play_1)
-
-
+    mystic = await message.reply_text(
+        _["play_2"].format(channel) if channel else _["play_1"]
     )
     plist_id = None
     slider = None
