380c8dc9f31583b4b3681a36c109cea266fc2918
slayer <meerutuniversity996@gmail.com>
2025-06-26 10:59:36 +0530

    Update play.py

--- AnieXEricaMusic/plugins/play/play.py
+++ AnieXEricaMusic/plugins/play/play.py
@@ -54,9 +54,13 @@ async def play_commnd(
     url,
     fplay,
 ):
+    # 🎵 Reply with a sticker on song request
+    await message.reply_sticker("CAACAgUAAyEFAASOlzVAAAEBZBhoXNqvZ-MQo8gQCaZbtac9AiJD-wACFQMAAnZPeFee-6uLME0avB4E")
+
     mystic = await message.reply_text(
         _["play_2"].format(channel) if channel else _["play_1"]
     )
+
     plist_id = None
     slider = None
     plist_type = None
